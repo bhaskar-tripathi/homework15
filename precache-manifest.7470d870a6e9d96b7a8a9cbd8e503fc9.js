094c3d0eccffeae7987d0564603e9e871f4cc342
Bhaskar Tripathi <bhaskar.tripathi@outlook.com>
2020-04-06 15:35:01 -0700

    Updates

---
+++ precache-manifest.7470d870a6e9d96b7a8a9cbd8e503fc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d1051bd6c281a017c506f05b417889",
+    "url": "/homework15/index.html"
+  },
+  {
+    "revision": "a58b8079e6af11eef7dd",
+    "url": "/homework15/static/css/main.13d68428.chunk.css"
+  },
+  {
+    "revision": "594a2a9bccba4e9db6d9",
+    "url": "/homework15/static/js/2.c8b9bcff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework15/static/js/2.c8b9bcff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58b8079e6af11eef7dd",
+    "url": "/homework15/static/js/main.72115bd8.chunk.js"
+  },
+  {
+    "revision": "cd00bd425e34b03f6f72",
+    "url": "/homework15/static/js/runtime-main.b7e64b3a.js"
+  }
+]);
